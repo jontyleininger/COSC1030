bafc1c5c7c7dfa6276dc0d7937a3b8e57ffff750
[Jon <[jonleininger@westernwyoming.edu>
2025-02-02 12:10:39 -0700

    ready for grading:)

--- hello-world/main.cpp
+++ hello-world/main.cpp
@@ -1,7 +1,16 @@
 //
-// Name
-// Date
+// Name Jon Leininger
+// Date 2-2-25
 // Hello World Programming Project
 // COSC 1030
 //
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!";
+    return 0;
+}
+
 
