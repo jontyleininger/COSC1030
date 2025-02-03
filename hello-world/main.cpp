436fb6004679cbf4f389f29b7d493def957fd849
Jon Ty Leininger <jonleininger@westernwyoming.edu>
2025-02-02 20:24:09 -0700

    Final submission

--- hello-world/main.cpp
+++ hello-world/main.cpp
@@ -1,7 +1,14 @@
 //
 // Name
 // Date
-// Hello World Programming Project
+// Your First C++ Program
 // COSC 1030
 //
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!";
+    return 0;
+}
