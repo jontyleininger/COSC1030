3732371543f2325be5d4ce17ec97ed7aafe3b9aa
Jon Ty Leininger <jontyleininger8@gmail.com>
2025-02-09 10:28:49 -0700

    ready for grading:)

--- restaurant-bill/instructions.md
+++ restaurant-bill/instructions.md
@@ -5,3 +5,4 @@ Write a program that computes the tax and tip on a restaurant bill for a patron 
 __Hint: Use the value 0.0675 to represent 6.75 percent, and 0.20 to represent 20 percent.__
 
 Review [Solving the Restaurant Bill Problem](https://mediaplayer.pearsoncmg.com/assets/gaddis_cpp10e_0204_Solving_Restaurant_Bill_Problem) VideoNote. You will see the output you should have for this programming challenge as well as the code.
+
--- restaurant-bill/main.cpp
+++ restaurant-bill/main.cpp
@@ -1,11 +1,31 @@
 //
-// Name
-// Date
+// Jon Leininger
+// 2-9-25
 // Restaurant Bill Programming Project
 // COSC 1030
 //
 
+#inlcude <iostream>
+using namespace std;
+
+
+int main ()
+{
 // Variable declarations
+	double meal = 88.67;
+	double tax = meal * 0.0675:
+	double subtotal = meal + tax:
+	double tip = subtotal * 0.20;
+	double total = subtotal + tip:
+	
+	cout << "Meal cost: " << meal << endl:
+	cout << "Tax: " << tax << endl;
+	cout << "Tip: " " << tip << endl:
+	cout << "Total: " << total << endl;
+
+	return 0;
+
+	
 
 // Constants for the tax rate and tip percentage
 
@@ -18,3 +38,5 @@
 // Calculate the total of the sale.
 
 // Print information about the sale.
+
+}
