489b1ae12ec8de3bfd65b4b29230eb4ff2d44596
Jon Ty Leininger <jontyleininger8@gmail.com>
2025-02-09 14:37:13 -0700

    ready for grading

--- ocean-levels/main.cpp
+++ ocean-levels/main.cpp
@@ -1,8 +1,38 @@
 //
-// Name
-// Date
+// Jon Leininger
+// 2-9-25
 // Ocean Levels Programming Project
 // COSC 1030
 //
+
+#include <iostream>
+using namespace std;
+
+
+int main () 
+{
+	
+	double seaRiseRate = 1.5 ; 
+	double seaLevel5 = seaRiseRate * 5 ;
+	double seaLevel7 = seaRiseRate * 7 ; 
+	double seaLevel0 = seaRiseRate * 10 ;
+
+
+	
+	cout << "The sea level in 5 years will be " << seaLevel5 << " millimeters higher than the current level." << endl;
+
+	cout << "The sea level in 7 years will be " << seaLevel7 << " millimeters higher than the current level." << endl;
+
+	cout << "The sea level in 10 years will be " << seaLevel0 << " millimeters higher than the current level." << endl;
+	
+
+	
+
+	
+
+
+
+return 0;
+}
 
 
