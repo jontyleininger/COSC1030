c5f3f8e08af7d97ccc7202aeb4608a2b2319f931
Jon Ty Leininger <jontyleininger8@gmail.com>
2025-02-18 20:49:21 -0700

    ready for grading :)

--- time-calculator/instructions.md
+++ time-calculator/instructions.md
@@ -7,3 +7,4 @@ Write a program that asks the user to enter a number of seconds.
 * There are 86,400 seconds in a day. If the number of seconds entered by the user is greater than or equal to 86,400, the program should display the number of days in that many seconds.
 
 Review [Solving the Time Calculator Problem](https://mediaplayer.pearsoncmg.com/assets/gaddis_cpp10e_0404_Solving_Time_Calculator_Problem) VideoNote. You will see the output you should have for this programming challenge as well as the code. 
+
--- time-calculator/main.cpp
+++ time-calculator/main.cpp
@@ -1,7 +1,65 @@
 //
-// Name
-// Date
+// Name Jon Leininger
+// Date 2-16-25
 // Time Calculator Programming Project
 // COSC 1030
 //
+
+#include <iostream>
+using namespace std;
+
+
+int main () 
+{
+
+long long int seconds;
+float secs_in_a_min;
+float secs_in_a_hour;
+float secs_in_a_day;
+secs_in_a_min = 60;
+secs_in_a_hour = 3600;
+secs_in_a_day = 86400;
+
+	
+cout << "Enter a Number of Seconds" << endl;
+cin >> seconds;
+
+if (seconds >= 60)
+{
+	if (seconds >= 3600)
+{
+		if (seconds >= 86400)
+			cout << "That is " << seconds/secs_in_a_day << " days" << endl;
+		else 	
+{	
+			cout << "That is " << seconds/secs_in_a_hour << " hour/s." << endl;
+}
+			
+}
+
+	else		
+		cout << "That is " << seconds/secs_in_a_min << " minutes." << endl;
+}
+else
+	cout << "That is " << seconds << " seconds" << endl;
+
+	
+
+	
+			
+	
+	
+	
+	
+	
+
+ 
+
+
+
+
+
+
+return 0;
+}
 
