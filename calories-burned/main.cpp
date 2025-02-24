c6aeec8d873cc7c39a1c990712af06ff86e991ae
Jon Ty Leininger <jontyleininger8@gmail.com>
2025-02-23 22:06:32 -0700

    ready for grading

--- calories-burned/main.cpp
+++ calories-burned/main.cpp
@@ -1,7 +1,37 @@
 //
-// Name
-// Date
+// Name Jon Leininger
+// Date 2-22-2025
 // Calories Burned Programming Project
 // COSC 1030
 //
 
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	const double cals = 3.9; 
+	double burnedCals;
+	int minutes;
+	
+	for (minutes = 5; minutes <= 30; minutes += 5)
+	{
+		burnedCals = minutes * cals;
+		cout << "You burned " << burnedCals << " calories in " << minutes << 
+		" minutes." << endl;
+	}
+return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
