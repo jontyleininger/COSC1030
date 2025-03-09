e4c19019a4e0d4c410ef54373363cee65a05bfbc
Jon Ty Leininger <jontyleininger8@gmail.com>
2025-03-09 12:56:24 -0600

    ready for grading

--- pennies-for-pay/main.cpp
+++ pennies-for-pay/main.cpp
@@ -5,4 +5,53 @@
 // COSC 1030
 //
 
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main ()
+{
+	
+	int days;
+	double pay; 
+	
+	cout << "How many days? " << endl;
+	cin >> days;
+	
+	
+	if (days < 1)
+	{
+	cout << "Please enter at least 1 for days." << endl;
+	cin >> days;
+	}
+
+
+	for (int i = 0; i < days; ++i)
+		pay +=./ pow(2,i);
+		
+	cout << "The total pay after " << days << " days: $" << fixed << setprecision(2) << pay /100.0 << endl;
+
+
+
+	
+
+		
+	
+		
+		
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
+
 
