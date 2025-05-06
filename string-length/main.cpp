ed5dff969c1e09c2d2c8bd958e70934a8058991c
Jon Ty Leininger <jontyleininger8@gmail.com>
2025-05-06 17:22:38 -0600

    ready for grading

--- string-length/main.cpp
+++ string-length/main.cpp
@@ -1,7 +1,37 @@
 //
-// Name
-// Date
+// Jon Leininger
+// 11-5
 // String Length Programming Project
 // COSC 1030
 //
+#include <iostream>
+using namespace std;
 
+
+int countChars(const char*);
+
+int main() 
+{
+	char userInput[100];  
+
+    	cout << "Enter a string: ";
+    	cin.getline(userInput, 100); 
+
+    	int length = countChars(userInput);
+   	 cout << "The number of characters in the string is: " << length << endl;
+
+   	return 0;
+}
+
+
+int countChars(const char* str) 
+{
+	int count = 0;
+    
+    	while (str[count] != '\0') 
+    	{
+        	count++;
+    	}
+    
+    	return count;  
+}
